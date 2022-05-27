a1ce347a1d0ad8db98680af187eef5f8987c34e6
Nicholas Wierenga <NicholasWierenga@gmail.com>
2022-05-27 01:19:48 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './MenuItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChirpusPizza';
+  items: MenuItem[] = [
+    {item: `Grilled Chicken`, category: `dinner`, price: 11.99},
+    {item: `Pizza`, category: `dinner`, price: 11.99},
+    {item: `Wings`, category: `sides`, price: 8.99},
+    {item: `Fries`, category: `sides`, price: 2.99},
+    {item: `Ceasar Salad`, category: `salads`, price: 5.99},
+    {item: `Cinnamon Roll`, category: `dessert`, price: 8.99}
+  ];
 }
